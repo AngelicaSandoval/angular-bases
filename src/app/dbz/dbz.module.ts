659dff1a11e2168cc5cc804ddfd87e4aae37b39b
Angelica Sandoval <angelica.sandoval@citytech.ec>
2024-11-13 09:18:09 -0500

    add-character

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
 import { ListComponent } from './components/list/list.component';
 import { CounterModule } from "../counter/counter.module";
+import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 
 @NgModule({
   declarations: [
     MainPageComponent,
-    ListComponent
+    ListComponent,
+    AddCharacterComponent
   ],
   exports: [
     MainPageComponent
